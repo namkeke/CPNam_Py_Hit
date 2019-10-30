564160da570570378a88694344b5ab848df8f60e
PNam <phuongnamhn2000@gmail.com>
2019-10-30 09:33:00 +0700

    week3_1

--- week 3/week3_1/bai2.py
+++ week 3/week3_1/bai2.py
@@ -1,10 +1,10 @@
 
 a, b = list(map(int, input().split())), list(map(int, input().split()))
+Sa = abs((a[0]-a[2])*(a[1]-a[3]))
+Sb = abs((b[0]-b[2])*(b[1]-b[3]))
 if a[2] <= b[0] or b[2] <= a[0] or a[3] <= b[1] or b[3] <= a[1]:
-    print("+oo")
+    print(Sa+Sb)
 else:
-    Sa = abs((a[0]-a[2])*(a[1]-a[3]))
-    Sb = abs((b[0]-b[2])*(b[1]-b[3]))
     Sab = (min(a[2], b[2]) - max(a[0], b[0])) * \
         (min(a[3], b[3]) - max(a[1], b[1]))
-    print(Sa+Sb if Sab <= 0 else Sa+Sb-2*Sab)
+    print(Sa+Sb-2*Sab)
